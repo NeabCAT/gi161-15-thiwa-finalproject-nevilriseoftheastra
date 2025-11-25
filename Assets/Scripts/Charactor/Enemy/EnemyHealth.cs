ea19f6bf3cb87a5fe4e61594fbcbbeab8d64e62d
NeabCAT <neabkub01@gmail.com>
2025-11-26 00:13:40 +0700

    player dash and flash and knockback

--- Assets/Scripts/Charactor/Enemy/EnemyHealth.cs
+++ Assets/Scripts/Charactor/Enemy/EnemyHealth.cs
@@ -5,12 +5,16 @@ using UnityEngine;
 public class EnemyHealth : MonoBehaviour
 {
     [SerializeField] private int startingHealth = 3;
+    [SerializeField] private GameObject deathVFXPrefab;
+    [SerializeField] private float knockBackThrust = 15f;
 
     private int currentHealth;
     private Knockback knockback;
+    private Flash flash;
 
     private void Awake()
     {
+        flash = GetComponent<Flash>();
         knockback = GetComponent<Knockback>();
     }
 
@@ -22,15 +26,22 @@ public class EnemyHealth : MonoBehaviour
     public void TakeDamage(int damage)
     {
         currentHealth -= damage;
-        Debug.Log(currentHealth);
-        knockback.GetKnockedBack(PlayerController.Instance.transform, 15f);
+        knockback.GetKnockedBack(PlayerController.Instance.transform, knockBackThrust);
+        StartCoroutine(flash.FlashRoutine());
+        StartCoroutine(CheckDetectDeathRoutine());
+    }
+
+    private IEnumerator CheckDetectDeathRoutine()
+    {
+        yield return new WaitForSeconds(flash.GetRestoreMatTime());
         DetectDeath();
     }
 
-    private void DetectDeath()
+    public void DetectDeath()
     {
         if (currentHealth <= 0)
         {
+            Instantiate(deathVFXPrefab, transform.position, Quaternion.identity);
             Destroy(gameObject);
         }
     }
