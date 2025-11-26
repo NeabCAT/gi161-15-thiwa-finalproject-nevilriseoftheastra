b639afe279b852fceb208d3d3febb61aaa4bca8f
NeabCAT <neabkub01@gmail.com>
2025-11-27 06:02:23 +0700

    AreaExit2

---
+++ Assets/Scripts/SceneManagement/CameraController.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Cinemachine;
+using UnityEngine;
+
+public class CameraController : Singleton<CameraController>
+{
+    private CinemachineCamera cinemachineCamera;
+    public void SetPlayerCameraFollow()
+    {
+        cinemachineCamera = FindObjectOfType<CinemachineCamera>();
+        cinemachineCamera.Follow = PlayerController.Instance.transform;
+    }
+}
